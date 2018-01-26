14bfd20bf212c597d19430bf034a6fe07fe070d2
tsou88 <35821929+tsou88@users.noreply.github.com>
2018-01-26 14:23:22 +0800

    Update More JOIN operations #13.sql

--- More JOIN operations #13.sql
+++ More JOIN operations #13.sql
@@ -1,3 +1,5 @@
+/*#13 Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles.*/
+
 SELECT name 
 FROM movie JOIN casting ON movie.id = movieid JOIN actor ON actor.id = actorid
 WHERE ord = 1
